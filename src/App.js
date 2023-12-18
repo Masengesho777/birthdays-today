6116380577f82129d7e597a4aa8862c6bad10d59
masengeshocadeau <masengeshocadeau603@gmail.com>
2023-12-18 13:15:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [peaple, setpeaple] = useState(data);
+  return( 
+  <main>
+    <section className='container'>
+      <h3>{peaple.length} birthdays today</h3>
+      <List peaple = {peaple}/>
+      <button onClick={()=>setpeaple([]) }>clear all</button>
+    </section>
+  </main>
   );
 }
 
